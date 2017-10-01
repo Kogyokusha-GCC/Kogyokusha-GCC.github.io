1ac7b8d1ac10162eff926aa28172e7d6ceca1a7f
stepney141 <stepney141andi.c.i@gmail.com>
2017-10-02 02:08:55 +0900

    first commiting
    はじめてりぽじとりをつくりました

---
+++ js/footer.js
@@ -0,0 +1,31 @@
+function isSmartphone() {   //スマホからのアクセスかどうか
+    var ua = navigator.userAgent;
+    if (ua.indexOf('Android') == -1 &&
+            ua.indexOf('iPhone') == -1 &&
+            ua.indexOf('iPad') == -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+document.open();
+document.write("<div class=\"footer-text mpuls-1m-medium text-center\">");
+document.write("<a href=\"index.html\">トップページ</a><br>");
+if (isSmartphone()) {
+    document.write("<a href=\"http://www.kogyokusha.ed.jp/m/index.shtml\">攻玉社学園公式サイト</a><br><br>");
+} else {
+    document.write("<a href=\"http://www.kogyokusha.ed.jp/index.shtml\">攻玉社学園公式サイト</a><br><br>");
+}
+
+document.write("</div>");
+document.write("<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-lang=\"ja\" data-hashtags=\"攻玉社\" data-url=\"http://comb21.web.fc2.com/\">ツイート</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>");
+document.write("<iframe src=\"https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.facebook.com%2Fkigyokusai%2F&layout=button&mobile_iframe=true&width=61&height=20&appId\" width=\"61\" height=\"20\" style=\"border:none;overflow:hidden\" scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\"></iframe>");
+
+document.write("<div class=\"row\">");
+document.write("<div class=\"col-lg-12\">");
+document.write("　<p>Copyright &copy; Kogyokusha Computer Club All Rights Reserved.</p>");
+document.write("</div>");
+document.write("</div>");
+document.write("<span id=\"page-top\" class=\"right\"><a href=\"#\">▲PAGE TOP</a></span>");
+document.close();
---
+++ js/menu.js
@@ -0,0 +1,23 @@
+document.open();
+
+document.write("<li>");
+document.write("<a href=\"http://comb21.web.fc2.com\">TOP</a>");
+document.write("</li>");
+
+document.write("<li>");
+document.write("<a href=\"activities.html\">活動内容</a>");
+document.write("</li>");
+
+document.write("<li>");
+document.write("<a href=\"download.html\">作品一覧</a>");
+document.write("</li>");
+
+document.write("<li>");
+document.write("<a href=\"link.html\">リンク</a>");
+document.write("</li>");
+
+document.write("<li>");
+document.write("<a href=\"inquiry.html\">お問い合わせ</a>");
+document.write("</li>");
+
+document.close();
