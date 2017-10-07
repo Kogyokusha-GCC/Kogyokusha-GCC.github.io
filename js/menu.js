086da0a97862d94ea1d95a52103ebef5f85a4ceb
stepney141 <stepney141andi.c.i@gmail.com>
2017-10-07 16:03:33 +0900

    修正
    ・html5準拠で書き換え
    ・news.htmlを一部cssファイルに分離、色を変更
    ・test.htmlを削除

--- js/menu.js
+++ js/menu.js
@@ -9,12 +9,12 @@ document.write("<a href=\"activities.html\">活動内容</a>");
 document.write("</li>");
 
 document.write("<li>");
-document.write("<a href=\"download.html\">作品一覧</a>");
+document.write("<a href=\"download.html\">配布ゲーム</a>");
 document.write("</li>");
 
-document.write("<li>");
-document.write("<a href=\"link.html\">リンク</a>");
-document.write("</li>");
+// document.write("<li>");
+// document.write("<a href=\"link.html\">リンク</a>");
+// document.write("</li>");
 
 document.write("<li>");
 document.write("<a href=\"inquiry.html\">お問い合わせ</a>");
