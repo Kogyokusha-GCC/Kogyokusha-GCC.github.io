3a1024193fa57983bc6cbd0b6a3a2347b72ac892
ゆみや(stepney141) <stepney141andi.c.i@gmail.com>
2017-10-15 11:41:53 +0900

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -19,12 +19,12 @@ if (isSmartphone()) {
 }
 
 document.write("</div>");
-document.write("<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-lang=\"ja\" data-hashtags=\"攻玉社\" data-url=\"http://comb21.web.fc2.com/\">ツイート</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>");
+document.write("<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-lang=\"ja\" data-hashtags=\"攻玉社\" data-url=\"https://kogyokusha-gcc.github.io\">ツイート</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>");
 document.write("<iframe src=\"https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.facebook.com%2Fkigyokusai%2F&layout=button&mobile_iframe=true&width=61&height=20&appId\" width=\"61\" height=\"20\" style=\"border:none;overflow:hidden\" scrolling=\"no\" frameborder=\"0\" allowTransparency=\"true\"></iframe>");
 
 document.write("<div class=\"row\">");
 document.write("<div class=\"col-lg-12\">");
-document.write("　<p>Copyright &copy; Kogyokusha Computer Club All Rights Reserved.</p>");
+document.write("　<p>Copyright &copy; Kogyokusha G.C.C. All Rights Reserved.</p>");
 document.write("</div>");
 document.write("</div>");
 document.write("<span id=\"page-top\" class=\"right\"><a href=\"#\">▲PAGE TOP</a></span>");
