43a449a755e972b847edac13a4e28ed9ec5fb9a2
stepney141 <stepney141andi.c.i@gmail.com>
2017-11-17 02:53:32 +0900

    取るに足らない修正 Part6

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.open();
 
 document.write(
     '<header id="header">',
-    '<h1>攻玉社学園 コンピュータ部',
+    '<h1>攻玉社中学・高校 コンピュータ部',
     '<br><small>KOGYOKUSHA Computer Club - GCC</small></h1>',
     '</header>'
 );
