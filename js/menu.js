371cbc2b306f55ae4f0ccd7d8eea378e17267564
ゆみや(stepney141) <stepney141andi.c.i@gmail.com>
2017-12-03 13:43:00 +0900

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,23 @@
 document.open();
 
 document.write("<li>");
-document.write("<a href=\"index.html\">トップページ</a>");
+document.write('<a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>トップページ</a>');
 document.write("</li>");
 
 document.write("<li>");
-document.write("<a href=\"activities.html\">活動内容</a>");
+document.write('<a href="activities.html">活動内容</a>');
 document.write("</li>");
 
 document.write("<li>");
-document.write("<a href=\"product.html\">部員作品</a>");
+document.write('<a href="product.html">部員作品</a>');
 document.write("</li>");
 
 document.write("<li>");
-document.write("<a href=\"link.html\">リンク</a>");
+document.write('<a href="link.html">リンク</a>');
 document.write("</li>");
 
 document.write("<li>");
-document.write('<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>');
-document.write("<a href=\"mailto:gyokucomb@gmail.com\">お問い合わせ</a>");
+document.write('<a href="mailto:gyokucomb@gmail.com"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>お問い合わせ</a>');
 document.write("</li>");
 
 document.close();
