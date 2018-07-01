53791671f3beaa06e4223fe41b2cdda19bef5850
stepney141 <stepney141andi.c.i@gmail.com>
2018-07-02 02:05:30 +0900

    いろいろいじった

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ function isSmartphone() {   //スマホからのアクセスかどうか
         return true;
     }
 }
+
+$(window).on('load resize', function(){
+    // navbarの高さを取得する
+    var height = $('.navbar').height();
+    // bodyのpaddingにnavbarの高さを設定する
+    $('body').css('padding-top',height); 
+});
